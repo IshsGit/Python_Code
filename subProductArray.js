c602160e66b900ded5e775fc1b9ab95e5566d3ac
ishslinux <aishtahad@yahoo.com>
2023-06-27 12:00:00 -0400

    New commit for 2023-06-27 12:00:00

---
+++ subProductArray.js
@@ -0,0 +1,20 @@
+function numSubarrayProductLessThanK(nums: number[], k: number): number {
+  if (k <= 1) return 0;
+
+  let left = 0;
+  let count = 0;
+  let product = 1;
+
+  for (let right = 0; right < nums.length; right++) {
+    product *= nums[right];
+
+    while (product >= k) {
+      product /= nums[left];
+      left++;
+    }
+
+    count += right - left + 1;
+  }
+
+  return count;
+}
