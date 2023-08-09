374e1d73f6885d2d1dd521b4f253f513caec9c03
ishslinux <aishtahad@yahoo.com>
2023-08-09 12:00:00 -0400

    New commit for 2023-08-09 12:00:00

---
+++ sub_arr.js
@@ -0,0 +1,17 @@
+function maxSubarraySum(arr: number[], k: number): number {
+  let maxSum = 0;
+  let windowSum = 0;
+
+  for (let i = 0; i < k; i++) {
+    windowSum += arr[i];
+  }
+
+  maxSum = windowSum;
+
+  for (let i = k; i < arr.length; i++) {
+    windowSum = windowSum - arr[i - k] + arr[i];
+    maxSum = Math.max(maxSum, windowSum);
+  }
+
+  return maxSum;
+}
