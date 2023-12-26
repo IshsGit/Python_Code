9e7c7166d8e1fe3d11a2a8b471a33d8d776e250e
ishslinux <aishtahad@yahoo.com>
2023-12-26 00:00:00 -0500

    Intervals and meeting rooms

---
+++ c_intervals.py
@@ -0,0 +1,13 @@
+"""
+Problem 2: Minimum Meeting Rooms
+Given a list of time intervals representing meetings, determine the minimum number of meeting rooms required.
+def min_meeting_rooms(intervals):
+    # Your implementation here
+
+# Example Usage:
+meetings = [[0, 30], [5, 10], [15, 20]]
+result = min_meeting_rooms(meetings)
+print(result)
+2
+
+"""
